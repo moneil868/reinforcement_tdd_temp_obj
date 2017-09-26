07b3da46828942b0c337996cef7654f2df5a4ee6
Marlon O'Neil <hello@marlononeil.com>
2017-09-26 15:00:50 -0400

    Made changes necessary for 'test_initialize_fahrenheit' to execute successfully

--- temperature.rb
+++ temperature.rb
@@ -1,9 +1,15 @@
 class Temperature
 
-  def initialize(temp)
+  def initialize(options = {})
+    @temp_fahrenheit = options[:f]
+    @temp_celcius = options[:c]
   end
 
   def to_fahrenheit
+    if @temp_fahrenheit
+      return @temp_fahrenheit
+    else
+    end
   end
 
 
